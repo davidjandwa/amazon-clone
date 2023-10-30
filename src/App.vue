a168f3d9bfe44a9ad9790e0f547536952603159c
David Jandwa <davidmuruya@hotmail.com>
2023-10-30 14:12:07 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2 class="bg-violet-500 p-3 text-white font-semibold">Amazon Clone</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
